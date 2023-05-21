aa610da7d669db0269c5814bc262d6f13151585e
David Silveira <daviewave@gmail.com>
2023-05-21 18:05:11 -0400

    made the declaration and export in the same line of code

--- models/user.ts
+++ models/user.ts
@@ -19,6 +19,4 @@ const UserSchema = new Schema({
   },
 });
 
-const User = models.User || model('User', UserSchema);
-
-export default User;
+export default models.User || model('User', UserSchema);
