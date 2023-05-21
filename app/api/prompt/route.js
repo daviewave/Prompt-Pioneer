e0b1e571866c001105c2efd94135d1202736ce4a
David Silveira <daviewave@gmail.com>
2023-05-21 19:34:54 -0400

    added endpoint to GET all posts

---
+++ app/api/prompt/route.js
@@ -0,0 +1,16 @@
+import { connectToDatabase } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export const GET = async (request) => {
+  try {
+    await connectToDatabase();
+    const prompts = await Prompt.find({}).populate('creator');
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+    });
+  }
+};
