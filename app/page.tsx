24728a9bbe6494365d1f52e21bb77ab3d0c3d683
David Silveira <daviewave@gmail.com>
2023-05-21 19:36:13 -0400

    removed break tag causing weird alignment issues with mobile screens

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,6 @@ const Home: FC<Props> = () => {
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         <div>Discover & Share</div>
-        <br className="max-md:hidden" />
         <div>
           <span className="orange_gradient text-center">
             AI-Powered Prompts
