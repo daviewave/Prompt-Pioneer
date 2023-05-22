0330538be85802c1b226002b8008a7dc4600120e
David Silveira <daviewave@gmail.com>
2023-05-21 22:51:36 -0400

    added Profile components

---
+++ app/profile/page.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { FC, useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/Profile';
+import user from '@models/user';
+
+type ProfilePageProps = {};
+
+const profilePageDescription = `Welcome to your personal haven on Prompt Pioneers. This is your space to explore, create, and share an array of AI-powered prompts tailored to your creative process. Here, you can track your productivity, efficiency, and the milestones of your creative journey.`;
+
+const ProfilePage: FC<ProfilePageProps> = ({}) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      'Are you sure you want to delete this prompt?'
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        });
+
+        const filteredPosts = posts.filter((p: any) => p._id !== post.id);
+        setPosts(filteredPosts);
+
+        window.location.reload();
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(`/api/users/${session?.user?.id}/posts`);
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    };
+    if (session?.user?.id) fetchPosts();
+  }, [session?.user?.id]);
+
+  return (
+    <Profile
+      name="Big D"
+      desc={profilePageDescription}
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default ProfilePage;
---
+++ components/Profile.tsx
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+
+import PromptCard from './PromptCard';
+
+type ProfileProps = {
+  name: string;
+  desc: string;
+  data: any[];
+  handleEdit: (post: any) => void | any;
+  handleDelete: (post: any) => void | any;
+};
+
+const Profile: FC<ProfileProps> = ({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name}'s Profile</span>
+      </h1>
+
+      <p className="desc text-left">{desc}</p>
+
+      <div className="mt-10 prompt_layout">
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={() => {}}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
