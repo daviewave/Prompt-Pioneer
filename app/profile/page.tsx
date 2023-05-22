0fef90bda8b84b1b7a8ea7d9fd200009bc953a68
David Silveira <daviewave@gmail.com>
2023-05-22 14:30:06 -0400

    edited fetch function in profile to check if id param is passed in the request, in which case we bring up that users profile instead of the session users

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,7 +2,7 @@
 
 import { FC, useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import Profile from '@components/Profile';
 import user from '@models/user';
@@ -11,9 +11,10 @@ type ProfilePageProps = {};
 
 const profilePageDescription = `Welcome to your personal haven on Prompt Pioneers. This is your space to explore, create, and share an array of AI-powered prompts tailored to your creative process. Here, you can track your productivity, efficiency, and the milestones of your creative journey.`;
 
-const ProfilePage: FC<ProfilePageProps> = ({}) => {
+const ProfilePage: FC<ProfilePageProps> = () => {
   const router = useRouter();
   const { data: session }: any = useSession();
+  const searchParams = useSearchParams();
 
   const [posts, setPosts] = useState([]);
 
@@ -44,20 +45,27 @@ const ProfilePage: FC<ProfilePageProps> = ({}) => {
 
   useEffect(() => {
     const fetchPosts = async () => {
+      // The ID from the URL, or the session user's ID if the URL ID is not available
+      const userId = searchParams.get('id') || session?.user?.id;
+  
       try {
-        const response = await fetch(`/api/users/${session?.user?.id}/posts`);
+        const response = await fetch(`/api/users/${userId}/posts`);
         const data = await response.json();
         setPosts(data);
       } catch (error) {
         console.log('error: ', error);
       }
     };
-    if (session?.user?.id) fetchPosts();
-  }, [session?.user?.id]);
+  
+    // Fetch posts if either the URL ID or the session user's ID is available
+    if (searchParams.get('id') || session?.user?.id) {
+      fetchPosts();
+    }
+  }, [searchParams.get('id'), session?.user?.id]);
 
   return (
     <Profile
-      name={session?.user?.name}
+      name={searchParams.get('name') || session?.user?.id}
       desc={profilePageDescription}
       data={posts}
       handleEdit={handleEdit}
