335b1c61d8d6db803c859802e4524814b2cc8f0e
David Silveira <daviewave@gmail.com>
2023-05-21 22:47:06 -0400

    added endpont to get posts only from a particular user

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,19 @@
+import { connectToDatabase } from '@utils/database';
+import Prompt from '@models/prompt';
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDatabase();
+
+    const prompts = await Prompt.find({ creator: params.id }).populate(
+      'creator'
+    );
+    return new Response(JSON.stringify(prompts), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify(error), {
+      status: 500,
+    });
+  }
+};
