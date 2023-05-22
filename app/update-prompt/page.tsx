5ba1b64b3f556dd368e0dc755567544457260d30
David Silveira <daviewave@gmail.com>
2023-05-21 22:49:18 -0400

    added edit functionality using the update-prompt endpoint created

---
+++ app/update-prompt/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import Form from '@components/Form';
+
+type Props = {};
+
+const EditPrompt = (props: Props) => {
+  const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get('id');
+
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  useEffect(() => {
+    const getPromptDetails = async () => {
+      try {
+        const response = await fetch(`/api/prompt/${promptId}`);
+        const data = await response.json();
+        setPost({
+          prompt: data.prompt,
+          tag: data.tag,
+        });
+      } catch (error) {
+        console.log('error: ', error);
+      }
+    };
+
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
+  const updatePrompt = async (e: any) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    if (!promptId) return alert('Prompt ID not found');
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
+
+export default EditPrompt;
