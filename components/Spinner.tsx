9ab0a3079885ac90b0a8e27664ecf550a7b18077
David Silveira <daviewave@gmail.com>
2023-05-22 20:39:15 -0400

    added a spinner component for loading

---
+++ components/Spinner.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
+
+const Spinner = ({ message }) => {
+  return (
+    <>
+      <div className="flex justify-center items-center mr-2">
+        <Circles color="#00BFFF" height={50} width={50} />
+      </div>
+
+      {message && message !== '' && (
+        <p className="text-lg text-center px-2">{message}</p>
+      )}
+    </>
+  );
+};
+
+export default Spinner;
