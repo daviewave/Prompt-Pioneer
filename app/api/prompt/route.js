a5d96ee27ed1fa468408c7588f1216c529bbe288
David Silveira <daviewave@gmail.com>
2023-05-23 23:54:00 -0400

    added pagination to my posts endpoint

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,33 @@
 import { connectToDatabase } from '@utils/database';
 import Prompt from '@models/prompt';
 
-export const GET = async (request) => {
+export const GET = async (req, res) => {
+  let { limit = 9, page = 1 } = req.body || {};
+
   try {
     await connectToDatabase();
-    const prompts = await Prompt.find({}).populate('creator');
-    return new Response(JSON.stringify(prompts), {
+
+    // get query params and set defaults
+    limit = limit ? Number(limit) : 8;
+    page = page ? Number(page) : 1;
+
+    // calculate how many to skip
+    const skip = (page - 1) * limit;
+
+    // query the database
+    const prompts = await Prompt.find({})
+      .skip(skip)
+      .limit(limit)
+      .populate('creator');
+
+    // // get total count for pagination
+    const totalCount = await Prompt.countDocuments({});
+
+    return new Response(JSON.stringify({ prompts, totalCount }), {
       status: 200,
     });
   } catch (error) {
+    console.error(error);
     return new Response(JSON.stringify(error), {
       status: 500,
     });
