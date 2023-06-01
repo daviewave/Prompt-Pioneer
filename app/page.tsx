7cdb85d3b200658ae1eaaf540ebb3797296efa7f
David Silveira <daviewave@gmail.com>
2023-05-31 22:01:57 -0400

    made intro text give some instruction on how to use

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,20 @@ const Home: FC<Props> = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        <div>Discover & Share</div>
+        <div>Discover, Try & Share</div>
         <div>
-          <span className="orange_gradient text-center">
-            ChatGPT Prompts
-          </span>
+          <span className="orange_gradient text-center">ChatGPT Prompts</span>
         </div>
       </h1>
       <p className="desc text-center">
         Prompt Pioneer is a community-driven platform for discovering and
         sharing AI-generated prompts for your next creative project.
+      </p>
+
+      <p className="desc text-center">
+        Notice a prompt you'd like to try?
+        <br />
+        Click on it to generate a response directly from chatGPT.
       </p>
 
       <Feed />
