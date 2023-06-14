806c9ec5e5f2d5b2d887cb6c2287d76ec478efa4
David Silveira <daviewave@gmail.com>
2023-06-14 00:34:56 -0400

    fixed bug getting environment variables

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,8 @@ import GoogleProvider from 'next-auth/providers/google';
 import User from '@models/user';
 import { connectToDatabase } from '@utils/database';
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   throw new Error(
